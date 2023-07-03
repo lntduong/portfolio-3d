17a8ecad2d98710e3daa498c60cd40c5ca4daf84
Le Nguyen Thai Duong <lntduongit@gmail.com>
2023-07-03 16:56:08 +0700

    portfolio-3d first commit

---
+++ src/assets/index.js
@@ -0,0 +1,52 @@
+import logo from "./logo.svg";
+import backend from "./backend.png";
+import creator from "./creator.png";
+import mobile from "./mobile.png";
+import web from "./web.png";
+import github from "./github.png";
+import menu from "./menu.svg";
+import close from "./close.svg";
+
+import css from "./tech/css.png";
+import figma from "./tech/figma.png";
+import git from "./tech/git.png";
+import html from "./tech/html.png";
+import javascript from "./tech/javascript.png";
+import nodejs from "./tech/nodejs.png";
+import reactjs from "./tech/reactjs.png";
+import tailwind from "./tech/tailwind.png";
+import typescript from "./tech/typescript.png";
+
+import mekongdata from "./company/mekongdata.png";
+import vinx from "./company/vinx.png";
+import vpb from "./company/vpb.svg";
+
+import carrent from "./carrent.png";
+import jobit from "./jobit.png";
+import tripguide from "./tripguide.png";
+
+export {
+  logo,
+  backend,
+  creator,
+  mobile,
+  web,
+  github,
+  menu,
+  close,
+  css,
+  figma,
+  git,
+  html,
+  javascript,
+  nodejs,
+  reactjs,
+  tailwind,
+  typescript,
+  carrent,
+  jobit,
+  tripguide,
+  mekongdata,
+  vinx,
+  vpb
+};
